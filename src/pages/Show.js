cad1fb270ae7d34764132aefbda5e3567b30609e
david <d.spuli@gmx.at>
2021-10-18 15:19:00 +0200

    added show page

---
+++ src/pages/Show.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+      setShow(results);
+    });
+  }, [id]);
+
+  console.log('show', show);
+  return <div>This is the show page.{id}</div>;
+};
+
+export default Show;
